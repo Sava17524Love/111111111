e0ba9cdbd569c71eaea2a194b7335bee779873e7
Sava17524Love <144526618+Sava17524Love@users.noreply.github.com>
2024-11-16 13:23:59 +0200

    Add files via upload

--- titanic.py
+++ titanic.py
@@ -35,3 +35,20 @@ def fill_Gender(Gender):
 
 df["Gender"] = df["Gender"].apply(fill_Gender)
 
+X = df.drop('Survived', axis=1)
+y = df['Survived']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+classifier = KNeighborsClassifier(n_neighbors=60)
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+print('Відсоток правильно передбачених результатів: ', accuracy_score(y_test, y_pred) * 100)
+print('Confusion matrix: ')
+print(confusion_matrix(y_test, y_pred))
+
+
